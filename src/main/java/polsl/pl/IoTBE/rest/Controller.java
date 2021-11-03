25257cf03e8f8a2f56a7e9a8e6d973a887dda15b
krzpiec <43814080+krzpiec@users.noreply.github.com>
2021-11-04 00:46:16 +0100

    Nie można połączyć się do bazy danych

--- src/main/java/polsl/pl/IoTBE/rest/Controller.java
+++ src/main/java/polsl/pl/IoTBE/rest/Controller.java
@@ -1,15 +1,24 @@
 package polsl.pl.IoTBE.rest;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import polsl.pl.IoTBE.repository.TestRepository;
+import polsl.pl.IoTBE.repository.dao.TestEntity;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    private TestRepository testRepository;
+
     @GetMapping("/toja")
     public ResponseEntity<String> odp(){
+        TestEntity test = new TestEntity();
+        test.setName("stefan");
+        testRepository.save(test);
         return ResponseEntity.ok("JD");
     }
 
